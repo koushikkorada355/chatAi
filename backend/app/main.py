9c7027e0d03499fd46495f599be2b9723a33dcaa
Koushik <koushikkorada27@gmail.com>
2026-09-12 01:10:29 +0530

    final changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,12 +22,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173", # For local development
+        "https://chat-ai-chi-umber.vercel.app/login" # Replace with your actual Vercel URL!
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(conv_router, prefix="/conversations", tags=["Conversations"])
 app.include_router(chat_router, prefix="/conversations", tags=["Chat"])
